8c6c037c3ba34c9af63bf184e213456f4a838cbf
Fredrik Öhrström <oehrstroem@gmail.com>
2025-10-24 14:58:22 +0200

    Test all dbs.

--- tests/test.sh
+++ tests/test.sh
@@ -21,11 +21,6 @@ if [ -z "$OUTPUT_BASE" ]; then OUTPUT_BASE=build/test_output; fi
 
 tests/download_test_dbs.sh $SQLCOMP $OUTPUT_BASE $SOURCE $SINK
 
-echo "sqlserver->sqlserver"
-tests/test_run.sh $SQLCOMP ${OUTPUT_BASE}_sqlserver_sqlserver sqlserver sqlserver $FILTER
-
-exit 0
-
 echo "postgres->postgres"
 tests/test_run.sh $SQLCOMP ${OUTPUT_BASE}_postgres_postgres postgres postgres $FILTER
 
@@ -38,8 +33,8 @@ tests/test_run.sh $SQLCOMP ${OUTPUT_BASE}_postgres_mysql postgres mysql $FILTER
 echo "mysql->postgres"
 tests/test_run.sh $SQLCOMP ${OUTPUT_BASE}_mysql_postgres mysql postgres $FILTER
 
-
-exit 0
+echo "sqlserver->sqlserver"
+tests/test_run.sh $SQLCOMP ${OUTPUT_BASE}_sqlserver_sqlserver sqlserver sqlserver $FILTER
 
 echo "postgres->sqlserver"
 tests/test_run.sh $SQLCOMP ${OUTPUT_BASE}_postgres_mysql postgres sqlserver $FILTER
